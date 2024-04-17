163158ecf40740227cf75915708e549b72308601
Kcee Landon <landonkcee@gmail.com>
2024-04-17 12:10:56 -0700

    mern tutorial part 3

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 
 const express = require('express')
+const coasterRoutes = require('./routes/coasters')
 
 const app = express()
 
+// middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
+
+app.use('/api/coasters', coasterRoutes)
 
 // routes
 app.get('/', (req, res) => {
